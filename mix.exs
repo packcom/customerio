5804d2aa0cfa572155979391ca418c34fb6c14fb
Oskar Niburski <oskarniburski@gmail.com>
2018-10-27 19:53:25 -0700

    update(deps): version deps

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Customerio.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 0.13.0"},
+      {:httpoison, "~> 1.4.0"},
       {:ex_doc, "~> 0.0", only: :dev},
       {:poison, "~> 2.0 or ~> 3.0"},
       {:exvcr, "~> 0.10.2", only: :test},
