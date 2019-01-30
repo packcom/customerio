31ebba7bf4fd6cea90de4d89085c16cadea9555b
Tim Cooper <tim@kickback.com>
2019-01-29 16:36:33 -0800

    Upgrade httpoison to 1.5

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Customerio.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 1.4.0"},
+      {:httpoison, "~> 1.5"},
       {:ex_doc, "~> 0.0", only: :dev},
       {:poison, "~> 2.0 or ~> 3.0"},
       {:exvcr, "~> 0.10.2", only: :test},
